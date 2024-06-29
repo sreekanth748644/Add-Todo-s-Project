b67ca3f23f4b04112708ddb7cd29dbd40a455a44
srikanth <sreekanth220124@gmail.com>
2024-06-29 19:27:46 +0530

    3 app.js is added generally

---
+++ app.js
@@ -0,0 +1,12 @@
+    // selectors
+    const todoInput=document.querySelector(".todo-input");
+    const todoButton=document.querySelector(".todo-button");
+    const todoList=document.querySelector(".todo-list");
+    //event listeners
+    todoButton.addEventListener("click",addTodo);
+    // functions
+    function addTodo(e){
+        e.preventDefault();
+        console.log("hello")
+    }
+   
