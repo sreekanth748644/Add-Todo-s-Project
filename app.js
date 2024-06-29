c15ece73b95d40429e21fb84e1106191ede17606
srikanth <sreekanth220124@gmail.com>
2024-06-29 19:58:51 +0530

    4. buttons and divs and li added

--- app.js
+++ app.js
@@ -7,6 +7,26 @@
     // functions
     function addTodo(e){
         e.preventDefault();
-        console.log("hello")
+       
+        // add div 
+        const todoDiv=document.createElement("div");
+        todoDiv.classList.add("todo");
+        //create a li to our div
+        const newtodo=document.createElement("li");
+        newtodo.classList.add("todo-item");
+        newtodo.innerText="hello";
+        todoDiv.appendChild(newtodo);
+        //creating button
+        const completedButton=document.createElement('button');
+        completedButton.innerHTML='<i class="fas fa-check"></i>';
+        completedButton.classList.add("complete-btn")
+        todoDiv.appendChild(completedButton);
+       
+        //creating trash button
+        const trashButton=document.createElement('button');
+       trashButton.classList.add("trash-btn")
+        trashButton.innerHTML='<i class="fas fa-trash"></i>';
+        todoDiv.appendChild(trashButton);
+        todoList .appendChild(todoDiv);
     }
    
